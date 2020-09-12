32be5137362ba11f15c5bdab99aed5b03c14f699
Nelson Foltz <nelsfoltz@gmail.com>
2020-09-12 14:27:36 -0400

    add: binding for toggleLocked

--- src/Die.jsx
+++ src/Die.jsx
@@ -1,12 +1,12 @@
-import React, { Component } from "react";
-import "./Die.css";
+import React, { Component } from 'react';
+import './Die.css';
 
 class Die extends Component {
   render() {
     return (
       <button
-        className={"Die"}
-        style={{ backgroundColor: this.props.locked ? "grey" : "black" }}
+        className={'Die'}
+        style={{ backgroundColor: this.props.locked ? 'grey' : 'black' }}
         onClick={this.props.handleClick}
       >
         {this.props.val}
